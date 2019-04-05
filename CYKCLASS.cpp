892e7760a136c3c47d53dfb5683845db172fc680
onibaku789 <varga.attila798@gmail.com>
2019-04-05 17:54:44 +0200

    It's working but at what cost?? REE

--- CYKCLASS.cpp
+++ CYKCLASS.cpp
@@ -7,28 +7,31 @@
 #include <cstring>
 #include "CYKCLASS.h"
 
-void CYKCLASS::parseGrammar(std::string & inputFileName) {
-    std::ifstream inputStream(inputFileName);
-    std::string line,tempString,prod;
+using Vec3 = std::vector<std::vector<std::vector<std::string>>>;
 
-    getline(inputStream,line);
+void CYKCLASS::parseGrammar(std::string &inputFileName) {
+    std::ifstream inputStream(inputFileName);
+    std::string line, tempString, prod;
+
+    getline(inputStream, line);
     startSymbol = line;
 
-    getline(inputStream,line);
-    std::istringstream iss (line);
 
-    while(iss >> tempString){
+    getline(inputStream, line);
+    std::istringstream iss(line);
+
+    while (iss >> tempString) {
         nonTerminals.emplace_back(tempString);
     }
 
-    getline(inputStream,line);
-    std::istringstream iss2 (line);
+    getline(inputStream, line);
+    std::istringstream iss2(line);
 
-    while(iss2 >> tempString){
+    while (iss2 >> tempString) {
         terminals.emplace_back(tempString);
     }
 
-    while(getline(inputStream,line)) {
+    while (getline(inputStream, line)) {
         std::istringstream iss3(line);
         iss3 >> prod;
         while (iss3 >> tempString) {
@@ -36,184 +39,211 @@ void CYKCLASS::parseGrammar(std::string & inputFileName) {
         }
     }
 }
+
 void CYKCLASS::test() {
-    std::cout << "STARTSYMBOL" <<std::endl << startSymbol<<std::endl;
+    std::cout << "STARTSYMBOL" << std::endl << startSymbol << std::endl;
 
     std::cout << "NONTERMINALS" << std::endl;
-    for(const auto & i:nonTerminals){
-        std::cout << i << " " ;
+    for (const auto &i:nonTerminals) {
+        std::cout << i << " ";
     }
-    std::cout<< std::endl;
-    std::cout << "TERMINALS" <<  std::endl;
-    for(const auto & i:terminals){
-        std::cout << i << " " ;
+    std::cout << std::endl;
+    std::cout << "TERMINALS" << std::endl;
+    for (const auto &i:terminals) {
+        std::cout << i << " ";
     }
-    std::cout<< std::endl;
-    std::cout << "GRAMMAR" <<  std::endl;
-    for(const auto & map:grammar) {
+    std::cout << std::endl;
+    std::cout << "GRAMMAR" << std::endl;
+    for (const auto &map:grammar) {
         std::cout << map.first << " ";
-        for (const auto & i:map.second) {
+        for (const auto &i:map.second) {
             std::cout << i << " ";
         }
         std::cout << std::endl;
     }
     std::cout << "RESULTTABLE" << std::endl;
-    for(const auto & vec:resultTable){
-        for(const auto & i:vec){
-            for(const auto & ii:i){
-            std::cout << ii<< ",";
+    for (const auto &vec:resultTable) {
+        for (const auto &i:vec) {
+            for (const auto &ii:i) {
+                std::cout << ii << ",";
             }
-        std::cout << " ";
+            std::cout << " ";
         }
         std::cout << std::endl;
     }
 }
 
- std::vector<std::vector<std::vector<std::string>>>  CYKCLASS::createCYKTable (){
-    long length =  wordToFind.length();
-     std::vector<std::vector<std::vector<std::string>>> cykTable ;
-     cykTable.clear();
+std::vector<std::vector<std::vector<std::string>>> CYKCLASS::createCYKTable() {
+    long length = wordToFind.length();
+    std::vector<std::vector<std::vector<std::string>>> cykTable;
+    cykTable.clear();
+    std::vector<std::string> kappa;
+    std::string test = "-";
+    kappa.push_back(test);
 
 
+    cykTable.push_back(std::vector<std::vector<std::string>>());
+    for (unsigned int ii = 0; ii < length; ii++) {
+        cykTable[0].push_back(kappa);
+
+    }
 
 
-     for(unsigned long i = 0; i < length+1 ;i++){
-         cykTable.push_back(std::vector<std::vector<std::string>>(length));
-         for(unsigned long ii = 0; ii < length-i+1; ii++){
-                cykTable[i].push_back(std::vector<std::string>(grammar.size()));
-             cykTable[i].resize(wordToFind.length()-i+1);
-             cykTable[i][ii].resize(grammar.size());
-         }
+    for (unsigned int i = 1; i <= length; i++) {
+        cykTable.push_back(std::vector<std::vector<std::string>>());
+        for (unsigned int ii = 0; ii < length - i + 1; ii++) {
+            cykTable[i].push_back(kappa);
 
+        }
 
-     }
-
-
-
+    }
 
 
     return cykTable;
 }
 
-void CYKCLASS::makeCYKTable( std::vector<std::vector<std::vector<std::string>>> & table){
+void CYKCLASS::makeCYKTable(std::vector<std::vector<std::vector<std::string>>> &table) {
 
-for(int i = 0; i <wordToFind.length(); i++ ) {
-    char tomb[2];
-    wordToFind.copy(tomb, 1, i);
-    std::string tempString(tomb);
-    std::cout << tempString << std::endl;
-    std::vector<std::string> temp(grammar.size());
-    temp = doProdVec(tempString);
-    table[0][i] =  temp;
-}
-    table[0].resize(wordToFind.length());
+    for (int i = 0; i < wordToFind.length(); i++) {
+        std::string tempString;
+        tempString.push_back(wordToFind[i]);
+        std::vector<std::string> tempVec;
+        tempVec.push_back(tempString);
+        table[0][i] = tempVec;
+    }
 
-for(int i = 0 ; i<wordToFind.length();i++){
-    if(i+1 <= wordToFind.length())
-    table[1][i] = doProdVec(table[0][i],table[0][i+1]);
+    for (int i = 0; i < wordToFind.length(); i++) {
+        std::string tempString;
+        tempString.push_back(wordToFind[i]);
 
-}
-table[1].resize(wordToFind.length()-1);
+        table[1][i] = doProdVec(tempString);
+
+    }
 
 
+    for (int i = 0; i < wordToFind.length(); i++) {
+        if (i + 1 <= wordToFind.length())
+            table[2][i] = doProdVec(table[1][i], table[1][i + 1]);
 
-   for(int i = 2; i < wordToFind.length()-1; i++){
-        for (int j = 0; j < wordToFind.length()-i  ; j++) {
-            for(int compareFrom = 0; compareFrom < i; compareFrom++) {
-               //std::cout << "downwards" << table[compareFrom][j] << std::endl;
-               //std::cout << table[i-compareFrom][j+compareFrom]<<std::endl;
-              // std::cout << "downwards: " << downwards << " diagonal: " << diagonals;
-                   // std::cout << "prodstirng" << table[compareFrom][j]+table[i-compareFrom][j+compareFrom] << std::endl;
+    }
 
-                   table[i][j] = doProdVec(table[compareFrom][j],table[i-compareFrom-1][j+compareFrom]);
+
+    std::vector<std::string> temp;
+    for (int i = 3; i < table.size(); i++) {
+        for (int j = 0; j < table[i].size(); j++) {
+            temp.clear();
+            for (int compareFrom = 1; compareFrom < i; compareFrom++) {
+                std::vector<std::string> temp2;
+                temp2 = doProdVec(table[compareFrom][j], table[i - compareFrom][j + compareFrom]);
+                //NEM JÓ CSAK MŰKÖDIK EKSDE
+                //TODO
+                if (!Contains(temp2, "-"))
+                    temp.insert(temp.end(), temp2.begin(), temp2.end());
+
 
             }
 
-
+            table[i][j] = temp;
         }
 
     }
+
 }
-std::vector<std::string> CYKCLASS::doProdVec(std::vector<std::string> prodStringsA, std::vector<std::string>prodStringsB){
-    std::vector<std::string> resultVector(grammar.size());
+
+std::vector<std::string>
+CYKCLASS::doProdVec(std::vector<std::string> prodStringsA, std::vector<std::string> prodStringsB) {
+    std::vector<std::string> resultVector;
     std::string tempString;
     bool found = false;
 
-    for (int j = 0; j < prodStringsA.size() ; ++j) {
-        for(int ii = 0 ; ii < prodStringsB.size(); ++ii){
-            tempString = prodStringsA[j]+prodStringsB[ii];
+    for (int j = 0; j < prodStringsA.size(); ++j) {
+        for (int ii = 0; ii < prodStringsB.size(); ++ii) {
+            tempString = prodStringsA[j] + prodStringsB[ii];
 
             for (auto map:grammar) {
                 for (const auto &i:map.second) {
-                   if (tempString == i) {
-                       found = true;
-                        std::cout << tempString << std::endl;
-                       resultVector.push_back(map.first);
-                   }
+                    if (tempString == i) {
+                        found = true;
+                        //  std::cout << counter++ << ". TALÁLAT: " << tempString << " BEÍROM: " << map.first << std::endl;
+                        resultVector.push_back(map.first);
+                    }
                 }
 
             }
         }
     }
-    std::cout << "kosz" << std::endl;
+    // std::cout << "kosz" << std::endl;
 
-        if(!found)
-            resultVector.push_back("-");
+    if (!found)
+        resultVector.push_back("-");
 
 
-    return  resultVector;
+    return resultVector;
 }
 
 
+std::string CYKCLASS::doProd(std::string a) {
 
-std::string CYKCLASS::doProd(std::string a){
 
-
-    for( auto  map:grammar){
-        for( const auto & i:map.second){
-            if(a == i){
+    for (auto map:grammar) {
+        for (const auto &i:map.second) {
+            if (i == a) {
                 return map.first;
             }
         }
     }
 
-    return  "-";
+    return "-";
 
 }
 
-std::vector<std::string> CYKCLASS::doProdVec(std::vector<std::string> & a){
+std::vector<std::string> CYKCLASS::doProdVec(std::vector<std::string> &a) {
     std::vector<std::string> tempStrings;
 
-    for( auto  map:grammar){
-        for( const auto & i:map.second){
-            for(auto vec:a){
-                if(vec == i)
-                tempStrings.push_back(map.first);
+    for (auto map:grammar) {
+        for (const auto &i:map.second) {
+            for (auto vec:a) {
+                if (vec == i)
+                    tempStrings.push_back(map.first);
+
             }
         }
     }
 
-    return  tempStrings;
+    return tempStrings;
 
 }
-std::vector<std::string> CYKCLASS::doProdVec(std::string a){
+
+std::vector<std::string> CYKCLASS::doProdVec(std::string a) {
     std::vector<std::string> tempStrings;
 
-    for( auto  map:grammar){
-        for( const auto & i:map.second){
-                if(a == i)
-                    tempStrings.push_back(map.first);
-            }
+    for (auto map:grammar) {
+        for (auto i:map.second) {
+            if (i == a)
+                tempStrings.push_back(map.first);
+
+        }
 
     }
 
-    return  tempStrings;
+    return tempStrings;
 
 }
-void  CYKCLASS::doTheMath() {
+
+void CYKCLASS::doTheMath() {
     parseGrammar(inputStream);
     resultTable = createCYKTable();
     makeCYKTable(resultTable);
 }
 
 
+const bool CYKCLASS::Contains(std::vector<std::string> &Vec, const std::string &Element) {
+    if (std::find(Vec.begin(), Vec.end(), Element) != Vec.end())
+        return true;
+
+    return false;
+}
+
+void CYKCLASS::viewCYKTable(CYKCLASS::Vec3) {
+    //TODO
+}
